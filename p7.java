637a57d3c14ae1c23d65be778867d9066fde4e02
anilsjr <anil.vishwakarma.it@gmail.com>
2024-09-17 12:52:06 +0530

    Update p7.java

--- p7.java
+++ p7.java
@@ -4,13 +4,13 @@ public class p7 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int tClass, aClass;
-        double percent;
+        float percent;
         System.out.println("Enter Total no of classes held :");
         tClass = s.nextInt();
         System.out.println("Enter no of classes you attended :");
         aClass = s.nextInt();
 
-        percent = 100 * aClass / tClass;
+        percent = 100.0f * aClass / tClass;
 
         if (percent < 75) {
             System.out.println("you are Not eligible to give exam");
